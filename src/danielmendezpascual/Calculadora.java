f25c694ee4c188d3d0dcf2c5e8d96e13ca328f0a
Daniel Méndez Pascual <dmendezp02@educarex.es>
2022-03-15 18:41:46 +0100

    Primeros ficheros añadidos

---
+++ src/danielmendezpascual/Calculadora.java
@@ -0,0 +1,31 @@
+package danielmendezpascual;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	Scanner teclado = new Scanner (System.in);
+	int num1, num2;
+	
+	System.out.println("Introduce el primer número: ");
+	num1 = teclado.nextInt();
+	
+	System.out.println("Introduce el segundo número: "); 
+	num2 = teclado.nextInt();
+	
+	System.out.println("La suma es: " +Calculadora.suma(num1, num2));
+	
+	System.out.println("La resta es: " +Calculadora.resta(num1, num2));
+	}
+
+	public static int suma (int num1, int num2) {
+		return (num1+num2);
+	}
+	
+	public static int resta (int num1, int num2) {
+		return (num1-num2);
+	}
+}
